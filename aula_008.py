f477fe2fa867073cf2ac4e425c80b3bd506af78c
Lucas Melo <oliveiramelo1996@gmai.com>
2023-08-14 10:29:47 -0300

    feito desafio 16

---
+++ aula_008.py
@@ -0,0 +1,7 @@
+from math import floor,trunc,sqrt;
+import emoji;
+import random;
+num = random.randint(1,10);
+print(num);
+print(emoji.emojize( f"A raiz quadrada de num é {trunc(sqrt(num))} emogi) 😀"));
+print("😀")
---
+++ desafios/desafio_016.py
@@ -0,0 +1,4 @@
+from math import trunc;
+num = float(input("Informe um numero: "));
+print(f"O valor digitado é {num} e sua porção inteira é {trunc(num)}");
+print(f"O valor digitado é {num} e sua porção inteira é {int(num)}");
